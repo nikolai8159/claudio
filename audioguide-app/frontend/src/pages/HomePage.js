77f09025be36428080c1815460bb46ae94c4a91f
nikolai8159 <nikolai8159@gmail.com>
2025-04-27 09:53:27 +0200

    Update HomePage.js

--- audioguide-app/frontend/src/pages/HomePage.js
+++ audioguide-app/frontend/src/pages/HomePage.js
@@ -4,10 +4,12 @@ function HomePage() {
   return (
     <div>
       <h1>Museum Audioguide</h1>
-      <p>Choose your museum here:</p>
-      <Link to="/museum/louvre">Louvre</Link><br />
-      <Link to="/museum/moma">MoMA</Link><br />
-      <Link to="/museum/uffizi">Uffizi Gallery</Link>
+      <p>Choose your museum!</p>
+      <ul>
+        <li><Link to="/museum/louvre">Louvre</Link></li>
+        <li><Link to="/museum/moma">MoMA</Link></li>
+        <li><Link to="/museum/uffizi">Uffizi Gallery</Link></li>
+      </ul>
     </div>
   );
 }
