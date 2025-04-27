ae08a2bc63b0dd309547d75c8cd28d5dd29e8002
nikolai8159 <nikolai8159@gmail.com>
2025-04-27 09:42:02 +0200

    Update App.js

--- audioguide-app/frontend/src/App.js
+++ audioguide-app/frontend/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MuseumPage from './pages/MuseumPage'; // we will create this one next!
+
 function App() {
   return (
-    <div>
-      <h1>Museum Audioguide</h1>
-      <p>Choose your museum!</p>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/museum/:museumId" element={<MuseumPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
