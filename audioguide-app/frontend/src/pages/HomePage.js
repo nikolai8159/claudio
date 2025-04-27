d21310e2269c31824ddaa7a331cc338df398e13b
nikolai8159 <nikolai8159@gmail.com>
2025-04-27 09:41:10 +0200

    Create HomePage.js

---
+++ audioguide-app/frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Museum Audioguide</h1>
+      <p>Choose your museum:</p>
+      <Link to="/museum/louvre">Louvre</Link><br />
+      <Link to="/museum/moma">MoMA</Link><br />
+      <Link to="/museum/uffizi">Uffizi Gallery</Link>
+    </div>
+  );
+}
+
+export default HomePage;
